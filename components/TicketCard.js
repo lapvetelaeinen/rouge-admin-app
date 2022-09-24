eb9f5c9ed7a3de5f8da1a753023839a8108f70f3
Filip Lapveteläinen <filip.lapvetelainen@gmail.com>
2022-09-24 20:38:53 +0200

    added more

--- components/TicketCard.js
+++ components/TicketCard.js
@@ -3,28 +3,38 @@ import Dots from "./svg-components/Dots";
 import Pen from "./svg-components/Pen";
 import Trash from "./svg-components/Trash";
 
-export default function TicketCard({ ticket }) {
+export default function TicketCard({ ticket, toggle, setSelectedTicketClass }) {
   const [showSettings, setShowSettings] = useState(false);
 
   let menuRef = useRef();
 
-
   useEffect(() => {
     let handler = (event) => {
-      if (!menuRef.current.contains(event.target)){
+      if (!menuRef.current.contains(event.target)) {
         setShowSettings(false);
       }
-    }
+    };
 
-    document.addEventListener('mousedown', handler);
+    document.addEventListener("mousedown", handler);
 
     return () => {
-      document.removeEventListener('mousedown', handler);
-    }
+      document.removeEventListener("mousedown", handler);
+    };
   });
-  
+
+  const handleTrash = () => {
+    setShowSettings(false);
+    toggle();
+  }
+
+  const handleDotClick = () => {
+    setShowSettings(true);
+    setSelectedTicketClass(ticket.ticketClass);
+  }
+
   return (
-    <div 
+    
+    <div
       key={ticket.eventName}
       className="bg-violet-300 p-5 rounded-md shadow-lg mt-4 text-lg flex justify-between"
     >
@@ -35,38 +45,42 @@ export default function TicketCard({ ticket }) {
         <p>Biljetter kvar: 587/800</p>
       </div>
       <div>
-
-     
-          <div ref={menuRef} className={`${!showSettings && 'hidden'} bg-violet-200 text-sm shadow-2xl flex flex-col`}>
-            <div className="settings-box flex justify-between bg-violet-200 p-4" onClick={() => console.log('yiha')}>
-
-            <p className="settings-box text-neutral-700">Ändra </p>
+        <div
+          ref={menuRef}
+          className={`${
+            !showSettings && "hidden"
+          } bg-violet-200 text-sm shadow-2xl flex flex-col`}
+        >
+          <div
+            className="settings-box flex justify-between bg-violet-200 p-4"
+            onClick={() => console.log("yiha")}
+          >
+            <p className="settings-box text-neutral-700">Ändra</p>
             <Pen
-            width={15}
-            height={15}
-            fill="#404040"
-            className="settings-box"
-          />
-            </div>
-            <div className="settings-box flex justify-between gap-2 bg-red-200 p-4">
-
+              width={15}
+              height={15}
+              fill="#404040"
+              className="settings-box"
+            />
+          </div>
+          <div className="settings-box flex justify-between gap-2 bg-red-200 p-4" onClick={() => handleTrash()}>
             <p className="settings-box text-neutral-500">Ta bort</p>
             <Trash
-            width={15}
-            height={15}
-            fill="#f87171"
-            onClick={() => console.log("hello")}
-            className="mt-1 settings-box"
-          />
-            </div>
+              width={15}
+              height={15}
+              fill="#f87171"
+              onClick={() => console.log("hello")}
+              className="mt-1 settings-box"
+            />
           </div>
-          <Dots
-        width={35}
-        height={35}
-        fill="#ddd6fe"
-        onClick={() => setShowSettings(true)}
-        className={showSettings && 'hidden'}
-      />
+        </div>
+        <Dots
+          width={35}
+          height={35}
+          fill="#ddd6fe"
+          onClick={() => handleDotClick()}
+          className={showSettings && "hidden"}
+        />
       </div>
     </div>
   );
--- pages/admin/tickets/single-ticket/[eventName].js
+++ pages/admin/tickets/single-ticket/[eventName].js
@@ -6,11 +6,12 @@ import Image from "next/image";
 import Times from "../../../../components/svg-components/Times";
 import TicketCard from "../../../../components/TicketCard";
 
-
 export default function TicketsPage({ eventInfo }) {
   const router = useRouter();
   const [allTickets, setAllTickets] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [selectedTicketClass, setSelectedTicketClass] = useState(null);
   const [showSettings, setShowSettings] = useState(false);
 
   const BUCKET_URL = "https://rouge-event-images.s3.eu-west-2.amazonaws.com/";
@@ -24,38 +25,60 @@ export default function TicketsPage({ eventInfo }) {
   } = useForm();
 
   const onSubmit = async (data) => {
-
-    
-    const params = 
-      {
-        eventName: eventInfo.eventName,
-        ticketClass: data.ticketClass,
-        price: data.price,
-        maxAmount: data.maxAmount,
-      };
+    const params = {
+      eventName: eventInfo.eventName,
+      ticketClass: data.ticketClass,
+      price: data.price,
+      maxAmount: data.maxAmount,
+    };
 
     if (data) {
-
-      axios.post("https://47yon8pxx3.execute-api.eu-west-2.amazonaws.com/rouge-api/create-ticket", params);
+      axios.post(
+        "https://47yon8pxx3.execute-api.eu-west-2.amazonaws.com/rouge-api/create-ticket",
+        params
+      );
       reset();
-
     } else {
       return;
     }
+    router.reload(window.location.pathname)
+
   };
 
-  useEffect(() => {
-    if(!allTickets){
-      axios.get(`https://47yon8pxx3.execute-api.eu-west-2.amazonaws.com/rouge-api/get-tickets?eventName=${eventInfo.eventName}`).then(res => setAllTickets(res.data));
-    } return;
+  const deleteTicket = () => {
+    const params = {
+      eventName: eventInfo.eventName,
+      ticketClass: selectedTicketClass
+    }
 
-    });
+
+    axios.post("https://47yon8pxx3.execute-api.eu-west-2.amazonaws.com/rouge-api/delete-ticket", params);
+
+
+    router.reload(window.location.pathname)
+    
+  }
+
+  useEffect(() => {
+    if (!allTickets) {
+      axios
+        .get(
+          `https://47yon8pxx3.execute-api.eu-west-2.amazonaws.com/rouge-api/get-tickets?eventName=${eventInfo.eventName}`
+        )
+        .then((res) => setAllTickets(res.data));
+    }
+    return;
+  });
 
   return (
     <div className="min-h-screen bg-slate-800 relative md:px-28 md:py-10">
-              {showModal ? (
-        <div className="bg-neutral-800 absolute z-50 h-full w-full flex justify-center items-center bg-opacity-80">
-          <div className="bg-neutral-200 w-full min-h-[300px] m-4 rounded-3xl mb-40">
+      {showModal ? (
+        <div className="">
+          <div
+            className="bg-neutral-800 z-40 absolute h-full w-full flex justify-center items-center bg-opacity-80"
+            onClick={() => setShowModal(false)}
+          ></div>
+          <div className="absolute bg-neutral-200 z-50 w-full min-h-[300px rounded-3xl p-4 mt-40">
             <div className="flex flex-col">
               <div className="flex justify-between p-1">
                 <p></p>
@@ -67,71 +90,99 @@ export default function TicketsPage({ eventInfo }) {
                 />
               </div>
               <form
-                    onSubmit={handleSubmit(onSubmit)}
-                    autoComplete="off"
-                    className="flex flex-col gap-3"
-                  >
-                    <input
-                      type="text"
-                      placeholder="Biljettklass"
-                      name="ticketClass"
-                      {...register("ticketClass")}
-                      className="p-4 bg-violet-300 placeholder-neutral-700 text-neutral-900 rounded-md shadow-sm"
-                    />
+                onSubmit={handleSubmit(onSubmit)}
+                autoComplete="off"
+                className="flex flex-col gap-3"
+              >
+                <input
+                  type="text"
+                  placeholder="Biljettklass"
+                  name="ticketClass"
+                  {...register("ticketClass")}
+                  className="p-4 bg-violet-300 placeholder-neutral-700 text-neutral-900 rounded-md shadow-sm"
+                />
 
-                    <input
-                      type="number"
-                      placeholder="Pris"
-                      name="price"
-                      {...register("price")}
-                      className="p-4 bg-violet-300 placeholder-neutral-700 text-neutral-900 rounded-md shadow-sm"
-                    />
+                <input
+                  type="number"
+                  placeholder="Pris"
+                  name="price"
+                  {...register("price")}
+                  className="p-4 bg-violet-300 placeholder-neutral-700 text-neutral-900 rounded-md shadow-sm"
+                />
 
-                    <input
-                      type="number"
-                      placeholder="Antal"
-                      name="maxAmount"
-                      {...register("maxAmount")}
-                      className="p-4 bg-violet-300 placeholder-neutral-700 text-neutral-900 rounded-md shadow-sm"
-                    />
+                <input
+                  type="number"
+                  placeholder="Antal"
+                  name="maxAmount"
+                  {...register("maxAmount")}
+                  className="p-4 bg-violet-300 placeholder-neutral-700 text-neutral-900 rounded-md shadow-sm"
+                />
 
-                      <input
-                        type="submit"
-                        className="p-4 bg-violet-600 placeholder-neutral-700 text-neutral-300 rounded-md shadow-sm"
-                        value="Skapa"
-                      />
-
-                  </form>
+                <input
+                  type="submit"
+                  className="p-4 bg-violet-600 placeholder-neutral-700 text-neutral-300 rounded-md shadow-sm"
+                  value="Skapa"
+                />
+              </form>
             </div>
           </div>
         </div>
       ) : null}
 
-        <div>
-          <Image
-            src={imagePath}
-            width={500}
-            height={500}
-            alt=""
-            className="rounded-md"
-          />
+{showDeleteModal ? (
+        <div className="">
+          <div
+            className="bg-neutral-800 z-40 absolute h-full w-full flex justify-center items-center bg-opacity-80"
+            onClick={() => setShowDeleteModal(false)}
+          ></div>
+          <div className="absolute bg-neutral-200 z-50 w-full min-h-[300px rounded-3xl p-4 mt-40">
+            <div className="flex flex-col">
+              <div className="flex justify-between p-1">
+                <p></p>
+                <Times
+                  width={50}
+                  height={50}
+                  fill="#f57971"
+                  onClick={() => setShowDeleteModal(!showDeleteModal)}
+                />
+              </div>
+              <div className="px-10">
+                <p className="text-xl text-center pt-4">Är du säker på att du vill radera denna biljettklass?</p>
+                <div className="flex justify-center gap-8 pt-8 pb-4">
+                  <button className="border-2 border-neutral-500 rounded-md text-md text-neutral-700 py-3 px-6">Behåll</button>
+                  <button className="bg-red-400 shadow-md rounded-md text-md py-3 px-6" onClick={(e) => deleteTicket(e)}>Radera</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="px-10">
+      ) : null}
 
+      <div>
+        <Image
+          src={imagePath}
+          width={500}
+          height={500}
+          alt=""
+          className="rounded-md"
+        />
+      </div>
+      <div className="px-3">
         <div className="text-5xl text-center pt-5">{eventInfo.eventName}</div>
-        <button className="bg-[#d57187] w-full p-5 text-xl rounded-lg" onClick={() => setShowModal(true)}>
-            Ny biljett
+        <button
+          className="bg-[#d57187] w-full p-5 text-xl rounded-lg"
+          onClick={() => setShowModal(true)}
+        >
+          Ny biljett
         </button>
         <div>
-        {allTickets
-          ? allTickets.map((ticket) => (
-            <TicketCard key={ticket.eventName} ticket={ticket}/>
-              
-            ))
-          : null}
+          {allTickets
+            ? allTickets.map((ticket) => (
+                <TicketCard key={ticket.eventName} ticket={ticket} toggle={() => setShowDeleteModal(!showDeleteModal)} setSelectedTicketClass={(ticketClass) => setSelectedTicketClass(ticketClass)}/>
+              ))
+            : null}
         </div>
-        </div>
-
+      </div>
     </div>
   );
 }
