16ce1c4fd54fb6e0a2aedfe29c0757c72f4abac0
Filip Lapveteläinen <filip.lapvetelainen@gmail.com>
2023-08-22 12:06:43 +0200

    fix reval

--- pages/api/aws.js
+++ pages/api/aws.js
@@ -25,6 +25,23 @@ export default async (req, res) => {
     console.log("message from lambda: ", msg);
 
     message = msg;
+
+    const revalidationPayload = ["/", "/events"];
+
+    const validateRes1 = await fetch(
+      `https://rougeumea.se/api/revalidate?secret=Rouge_Umea2021!`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(revalidationPayload),
+      }
+    );
+
+    const validateMsg1 = await validateRes1.json();
+
+    console.log("VALIDATION1: ", validateMsg1);
   }
 
   if (body.type === "editEvent") {
@@ -45,6 +62,26 @@ export default async (req, res) => {
     console.log("message from lambda: ", msg);
 
     message = msg;
+
+    const validateRes1 = await fetch(
+      `https://rougeumea.se/api/revalidate?secret=Rouge_Umea2021!&path=/`
+    );
+
+    const validateRes2 = await fetch(
+      `https://rougeumea.se/api/revalidate?secret=Rouge_Umea2021!&path=/event/${event.eventId}`
+    );
+
+    const validateRes3 = await fetch(
+      `https://rougeumea.se/api/revalidate?secret=Rouge_Umea2021!&path=/events`
+    );
+
+    const validateMsg1 = await validateRes1.json();
+    const validateMsg2 = await validateRes2.json();
+    const validateMsg3 = await validateRes3.json();
+
+    console.log("VALIDATION1: ", validateMsg1);
+    console.log("VALIDATION2: ", validateMsg2);
+    console.log("VALIDATION3: ", validateMsg3);
   }
 
   if (body.type === "deleteEvent") {
@@ -65,6 +102,26 @@ export default async (req, res) => {
     console.log("message from lambda: ", msg);
 
     message = msg;
+
+    const validateRes1 = await fetch(
+      `https://rougeumea.se/api/revalidate?secret=Rouge_Umea2021!&path=/`
+    );
+
+    const validateRes2 = await fetch(
+      `https://rougeumea.se/api/revalidate?secret=Rouge_Umea2021!&path=/event/${event.eventId}`
+    );
+
+    const validateRes3 = await fetch(
+      `https://rougeumea.se/api/revalidate?secret=Rouge_Umea2021!&path=/events`
+    );
+
+    const validateMsg1 = await validateRes1.json();
+    const validateMsg2 = await validateRes2.json();
+    const validateMsg3 = await validateRes3.json();
+
+    console.log("VALIDATION1: ", validateMsg1);
+    console.log("VALIDATION2: ", validateMsg2);
+    console.log("VALIDATION3: ", validateMsg3);
   }
 
   if (body.type === "createTicket") {
@@ -85,6 +142,18 @@ export default async (req, res) => {
     console.log("message from lambda: ", msg);
 
     message = msg;
+
+    const validateRes2 = await fetch(
+      `https://rougeumea.se/api/revalidate?secret=Rouge_Umea2021!&path=/event/${event.eventId}`
+    );
+
+    const validateMsg1 = await validateRes1.json();
+    const validateMsg2 = await validateRes2.json();
+    const validateMsg3 = await validateRes3.json();
+
+    console.log("VALIDATION1: ", validateMsg1);
+    console.log("VALIDATION2: ", validateMsg2);
+    console.log("VALIDATION3: ", validateMsg3);
   }
 
   res.status(200).json(message);
